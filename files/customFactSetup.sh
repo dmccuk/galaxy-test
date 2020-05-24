0318660e4640f9a3f612565350a3652537a12863
Cloud User <centos@ip-172-31-47-217.eu-west-1.compute.internal>
2020-05-24 11:31:00 +0000

    adding locat fact template

--- files/customFactSetup.sh
+++ files/customFactSetup.sh
@@ -4,7 +4,8 @@ EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availa
 EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//'`"
 AMI_ID=`curl -s http://169.254.169.254/latest/meta-data/ami-id`
 
-
+echo "Support_Team: Hadoop" >> /etc/ansible/facts.d/local.fact
+echo "Callout: 24-7" >> /etc/ansible/facts.d/local.fact
 echo "EC2_INSTANCE_TYPE: "$EC2_INSTANCE_TYPE >> /etc/ansible/facts.d/local.fact
 echo "EC2_AVAIL_ZONE: "$EC2_AVAIL_ZONE >> /etc/ansible/facts.d/local.fact
 echo "EC2_REGION: "$EC2_REGION >> /etc/ansible/facts.d/local.fact
@@ -18,19 +19,3 @@ case $EC2_AVAIL_ZONE in
 	 echo "environment: Dev" >> /etc/ansible/facts.d/local.fact
 	;;
 esac
-
-
-
-
-#which apt ; STATUS=$?
-
-#case $STATUS in
-#        0)
-#         PACKAGES=`dpkg -l | tail -n +6 | awk -F" " '{print $2"."$3","}'`
-#         echo "PACKAGES: "$PACKAGES >> /etc/ansible/facts.d/local.fact
-#        ;;
-#        *)
-#         PACKAGES=` for i in \`rpm -qa\`; do echo -n $i","; done`
-#         echo "PACKAGES: "$PACKAGES >> /etc/ansible/facts.d/local.fact
-#        ;;
-#esac
