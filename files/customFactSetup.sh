231d5ef7ea7accee2c77146b541eb534f97993c4
Cloud User <centos@ip-172-31-47-217.eu-west-1.compute.internal>
2020-05-24 10:35:45 +0000

    refactored

--- files/customFactSetup.sh
+++ files/customFactSetup.sh
@@ -2,12 +2,24 @@
 EC2_INSTANCE_TYPE=`curl -s http://169.254.169.254/latest/meta-data/instance-type`
 EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
 EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//'`"
+AMI_ID=`curl -s http://169.254.169.254/latest/meta-data/ami-id`
 
 
-#echo "EC2_INSTANCE_TYPE: "$EC2_INSTANCE_TYPE >> /tmp/local.fact
 echo "EC2_INSTANCE_TYPE: "$EC2_INSTANCE_TYPE >> /etc/ansible/facts.d/local.fact
-#echo "EC2_AVAIL_ZONE: "$EC2_AVAIL_ZONE >> /etc/ansible/facts.d/local.fact
-#echo "EC2_REGION: "$EC2_REGION >> /etc/ansible/facts.d/local.fact
+echo "EC2_AVAIL_ZONE: "$EC2_AVAIL_ZONE >> /etc/ansible/facts.d/local.fact
+echo "EC2_REGION: "$EC2_REGION >> /etc/ansible/facts.d/local.fact
+echo "AMI_ID: "$AMI_ID >> /etc/ansible/facts.d/local.fact
+
+case $EC2_AVAIL_ZONE in
+	eu-west-1b)
+	 echo "environment: Production" >> /etc/ansible/facts.d/local.fact
+	;;
+	*)
+	 echo "environment: Dev" >> /etc/ansible/facts.d/local.fact
+	;;
+esac
+
+
 
 
 #which apt ; STATUS=$?
